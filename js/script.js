bc111b57b9adf44b5f6b526d54e09e332e68a1e9
swikars1 <swikarsharma@gmail.com>
2018-12-03 01:28:36 +0545

    expandable changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-	$('.flextext1').click({
+	
+	
+	
+	
+	
+	$('.flextext').click(function(){
+		$('.flex-container').children().not($(this)).animate({
+			"width":"6.5%"}
+			,600
+			,"swing"
+			).find('img').hide();
+			
+		$('.flex-container').children().not($(this)).css({"font-size":"25px"});
+		$(this).animate({"width":"54.5%"}
+		,600
+		,"swing");
+		
+
+		$(this).find('img').hide();
+		$('#first').css({"background-color":"red"});
+		
 		
 	});
-});
+	
+});rr
