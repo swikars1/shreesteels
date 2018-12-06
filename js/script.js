231587b3713cb532de9f2ff70501bf9c1a8f99e9
swikars1 <swikarsharma@gmail.com>
2018-12-06 08:01:00 +0545

    8 attributes changes

--- js/script.js
+++ js/script.js
@@ -2,14 +2,15 @@ $(document).ready(function(){
 	
 	
 	
-	
-	
+		
 	$('.flextext').click(function(){
+		
+		
 		$notselected = $('.flex-container').children().not($(this));
 		$notselected.animate({
 			"width":"6.5%"},600,"swing")
 			.find('img')
-			.not($('.iconclass'))
+			.not($('.iconclass')).not($('.inside1'))
 			.hide(400);
 		$(this).find($('.iconclass'))
 		.hide(400);
@@ -18,9 +19,93 @@ $(document).ready(function(){
 		$(this).animate({"width":"54.4%"}
 		,600
 		,"swing");
-		$('.verticaltext1').css({"left":"8%","top":"39%","font-size":"24px"});
+		$('.verticaltext1').css({"left":"8%","top":"39%"});
+		
+		$('.verticaltext1,.verticaltext2,.verticaltext3,.verticaltext4,.verticaltext5,.verticaltext6,.verticaltext7,.verticaltext8').css({"font-size":"24px"});
+		
 		$('.iconclass').css({"width":"45px"});
 		var selectedid = $(this).attr('id');	
+	});
+	
+	$('#first').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside1.jpg'>");
+		$('#second').find('img').not($('.iconclass')).remove();
+		$('#third').find('img').not($('.iconclass')).remove();
+		$('#fourth').find('img').not($('.iconclass')).remove();
+		$('#fifth').find('img').not($('.iconclass')).remove();
+		$('#sixth').find('img').not($('.iconclass')).remove();
+		$('#seventh').find('img').not($('.iconclass')).remove();
+		$('#eighth').find('img').not($('.iconclass')).remove();
+	});
+		$('#second').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside2.jpg'>");
+		$('#first').find('img').not($('.iconclass')).remove();
+		$('#third').find('img').not($('.iconclass')).remove();
+		$('#fourth').find('img').not($('.iconclass')).remove();
+		$('#fifth').find('img').not($('.iconclass')).remove();
+		$('#sixth').find('img').not($('.iconclass')).remove();
+		$('#seventh').find('img').not($('.iconclass')).remove();
+		$('#eighth').find('img').not($('.iconclass')).remove();
+	});
+		$('#third').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside3.jpg'>");
+		$('#second').find('img').not($('.iconclass')).remove();
+		$('#first').find('img').not($('.iconclass')).remove();
+		$('#fourth').find('img').not($('.iconclass')).remove();
+		$('#fifth').find('img').not($('.iconclass')).remove();
+		$('#sixth').find('img').not($('.iconclass')).remove();
+		$('#seventh').find('img').not($('.iconclass')).remove();
+		$('#eighth').find('img').not($('.iconclass')).remove();
+	});
+		$('#fourth').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside4.jpg'>");
+		$('#second').find('img').not($('.iconclass')).remove();
+		$('#third').find('img').not($('.iconclass')).remove();
+		$('#first').find('img').not($('.iconclass')).remove();
+		$('#fifth').find('img').not($('.iconclass')).remove();
+		$('#sixth').find('img').not($('.iconclass')).remove();
+		$('#seventh').find('img').not($('.iconclass')).remove();
+		$('#eighth').find('img').not($('.iconclass')).remove();
+	});
+		$('#fifth').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside5.jpg'>");
+		$('#second').find('img').not($('.iconclass')).remove();
+		$('#third').find('img').not($('.iconclass')).remove();
+		$('#fourth').find('img').not($('.iconclass')).remove();
+		$('#first').find('img').not($('.iconclass')).remove();
+		$('#sixth').find('img').not($('.iconclass')).remove();
+		$('#seventh').find('img').not($('.iconclass')).remove();
+		$('#eighth').find('img').not($('.iconclass')).remove();
+	});
+		$('#sixth').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside6.jpg'>");
+		$('#second').find('img').not($('.iconclass')).remove();
+		$('#third').find('img').not($('.iconclass')).remove();
+		$('#fourth').find('img').not($('.iconclass')).remove();
+		$('#fifth').find('img').not($('.iconclass')).remove();
+		$('#first').find('img').not($('.iconclass')).remove();
+		$('#seventh').find('img').not($('.iconclass')).remove();
+		$('#eighth').find('img').not($('.iconclass')).remove();
+	});
+		$('#seventh').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside7.jpg'>");
+		$('#second').find('img').not($('.iconclass')).remove();
+		$('#third').find('img').not($('.iconclass')).remove();
+		$('#fourth').find('img').not($('.iconclass')).remove();
+		$('#fifth').find('img').not($('.iconclass')).remove();
+		$('#sixth').find('img').not($('.iconclass')).remove();
+		$('#first').find('img').not($('.iconclass')).remove();
+		$('#eighth').find('img').not($('.iconclass')).remove();
+	});
+			$('#eighth').click(function(){
+		$(this).append("<img class='inside1' src='./images/inside8.jpg'>");
+		$('#second').find('img').not($('.iconclass')).remove();
+		$('#third').find('img').not($('.iconclass')).remove();
+		$('#fourth').find('img').not($('.iconclass')).remove();
+		$('#fifth').find('img').not($('.iconclass')).remove();
+		$('#sixth').find('img').not($('.iconclass')).remove();
+		$('#seventh').find('img').not($('.iconclass')).remove();
+		$('#first').find('img').not($('.iconclass')).remove();
 	});
 	
 });
