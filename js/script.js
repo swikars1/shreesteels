2735fab31fe58c063e022d027437026474abbfa9
swikars1 <swikar.sharma@olenepal.org>
2018-12-06 13:51:22 +0545

    changes from office

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
-	
-	
-	
-		
+	$('.iconclass').css({"width":"55px"});
 	$('.flextext').click(function(){
-		
-		
+		$('.flextext img').css({"width":"100%","left":"0%"});
+		$('.coverdiv').css({"width":"100%","left":"0%"});
 		$notselected = $('.flex-container').children().not($(this));
+		$(this).find('p').hide();
+		$notselected.find('p').show();
 		$notselected.animate({
 			"width":"6.5%"},600,"swing")
 			.find('img')
 			.not($('.iconclass')).not($('.inside1'))
 			.hide(400);
+
 		$(this).find($('.iconclass'))
 		.hide(400);
 		$notselected.find($('.iconclass'))
@@ -19,12 +19,14 @@ $(document).ready(function(){
 		$(this).animate({"width":"54.4%"}
 		,600
 		,"swing");
-		$('.verticaltext1').css({"left":"8%","top":"39%"});
+		$('.verticaltext1').css({"left":"8%","top":"34%"});
+		$('.verticaltext4').css({"top":"17%"});
 		
-		$('.verticaltext1,.verticaltext2,.verticaltext3,.verticaltext4,.verticaltext5,.verticaltext6,.verticaltext7,.verticaltext8').css({"font-size":"24px"});
+		$('.verticaltext1,.verticaltext2,.verticaltext3,.verticaltext4,.verticaltext5,.verticaltext6,.verticaltext7,.verticaltext8').css({"font-size":"23px"});
 		
 		$('.iconclass').css({"width":"45px"});
-		var selectedid = $(this).attr('id');	
+		var selectedid = $(this).attr('id');
+
 	});
 	
 	$('#first').click(function(){
